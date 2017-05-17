6cabc3863eaa8954d46b2806defd5a727cce02dc
Thomas Baigneres <thomas.baigneres@cryptoexperts.com>
2017-05-17 10:42:27 +0200

    The dev environment now runs under SSL, with a fake self-signed certificate (*not* to be used in production, of course).

--- services/web-dev/dockerfile/run_dev.py
+++ services/web-dev/dockerfile/run_dev.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python3
 
+import ssl
+context = ssl.SSLContext(ssl.PROTOCOL_TLSv1_2)
+context.load_cert_chain('foobar.crt', 'foobar.key')
+
 from app import app
-app.run(debug=True, host='0.0.0.0')
+#app.run(debug=True, host='0.0.0.0')
+app.run(debug=True, host='0.0.0.0', ssl_context=context)
