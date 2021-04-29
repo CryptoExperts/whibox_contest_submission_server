de564608fd4eb96a0d953bc5d992cca5739f020e
Junwei Wang <me@jwa.ng>
2021-04-29 19:36:49 +0200

    Fix flot

--- services/web-dev/static/js/flot.js
+++ services/web-dev/static/js/flot.js
@@ -2,87 +2,57 @@ $(function() {
     var programs = getProgramsToPlot();
 
     function dataGrowth(start, end, factor) {
-	var minutes = Math.floor((end-start)/60);
-	return [...Array(minutes+1).keys()].map(i => [start*1000 + i*60000, factor*Math.pow(i/1440,2)]);
+        var minutes = Math.floor((end-start)/60);
+        return [...Array(minutes+1).keys()].map(i => [start*1000 + i*60000, factor*Math.pow(i/1440,2)]);
     }
 
     function dataDecrease(start, peak, end, factor) {
-	var minutes_before_peak = Math.floor((peak-start)/60);
-	var minutes_after_peak = Math.floor((end-peak)/60);
-	var minutes_decrease = Math.min(minutes_before_peak, minutes_after_peak);
-	return [...Array(minutes_decrease+1).keys()].map(i => [peak*1000 + i*60000, factor*Math.pow((minutes_before_peak-i)/1440,2)]);
+        var minutes_before_peak = Math.floor((peak-start)/60);
+        var minutes_after_peak = Math.floor((end-peak)/60);
+        var minutes_decrease = Math.min(minutes_before_peak, minutes_after_peak);
+        return [...Array(minutes_decrease+1).keys()].map(i => [peak*1000 + i*60000, factor*Math.pow((minutes_before_peak-i)/1440,2)]);
     }
 
     function getData() {
-	var now = Math.floor(Date.now() / 1000);
-	var deadline = Math.floor(new Date('2019-08-21T12:00:00Z').getTime() / 1000);
-	now = Math.min(now, deadline);
-	var strawberries = [];
-	var carrots = [];
-	programs.forEach(function(p) {
-	    var strawberry_curve;
-	    if (p.ts_break > 0) {
-		strawberry_curve1 = dataGrowth(p.ts_publish, p.ts_break, p.performance);
-		strawberry_curve2 = dataDecrease(p.ts_publish, p.ts_break, now, p.performance);
-		strawberry_curve = strawberry_curve1.concat(strawberry_curve2);
-	    } else {
-		strawberry_curve = dataGrowth(p.ts_publish, now, p.performance);
-	    }
-	    strawberries.push({
-		color: p.color,
-		label: "&nbsp;" + p.name + " (" + p.id + ")&nbsp;&nbsp;",
+        var now = Math.floor(Date.now() / 1000);
+        var deadline = Math.floor(new Date('2021-09-11T00:00:00Z').getTime() / 1000);
+        now = Math.min(now, deadline);
+        var strawberries = [];
+        programs.forEach(function(p) {
+            var strawberry_curve;
+            if (p.ts_break > 0) {
+                strawberry_curve1 = dataGrowth(p.ts_publish, p.ts_break, p.performance);
+                strawberry_curve2 = dataDecrease(p.ts_publish, p.ts_break, now, p.performance);
+                strawberry_curve = strawberry_curve1.concat(strawberry_curve2);
+            } else {
+                strawberry_curve = dataGrowth(p.ts_publish, now, p.performance);
+            }
+            strawberries.push({
+                color: p.color,
+                label: "&nbsp;" + p.name + " (" + p.id + ")&nbsp;&nbsp;",
                 data: strawberry_curve
-	    });
+            });
 
-	    var carrot_curve;
-	    ts_peak = now;
-	    if (p.ts_break > 0 || p.ts_invert > 0) {
-		if (p.ts_break > 0) {
-		    ts_peak = p.ts_break;
-		}
-		if (p.ts_invert > 0) {
-		    ts_peak = Math.min(p.ts_invert, ts_peak);
-		}
-		carrot_curve1 = dataGrowth(p.ts_publish, ts_peak, 0.5*p.performance);
-		carrot_curve2 = dataDecrease(p.ts_publish, ts_peak, now, 0.5*p.performance);
-		carrot_curve = carrot_curve1.concat(carrot_curve2);
-	    } else {
-		carrot_curve = dataGrowth(p.ts_publish, now, 0.5*p.performance);
-	    }
-
-	    carrots.push({
-		color: p.color,
-		label: p.name + " (" + p.id + ")&nbsp;&nbsp;",
-                data: carrot_curve
-	    });
         });
 
-	return [strawberries, carrots];
+        return [strawberries];
     }
 
     // Set up the control widget
     var updateInterval = 600000; // 10 min
     var data = getData();
     var plotStrawberry = $.plot("#strawberryholder", data[0] , {
-	series: {shadowSize: 0},
-	yaxis: {min: 0, position: "right"},
-	xaxis: { show: true, mode: "time", timeformat: "%m/%d" },
-	legend: { position: "nw", noColumns: 2, margin: 10}
-    });
-    var plotCarrot = $.plot("#carrotholder", data[1] , {
-	series: {shadowSize: 0},
-	yaxis: {min: 0, position: "right"},
-	xaxis: { show: true, mode: "time", timeformat: "%m/%d" },
-	legend: { position: "nw", noColumns: 2, margin: 10}
+        series: {shadowSize: 0},
+        yaxis: {min: 0, position: "right"},
+        xaxis: { show: true, mode: "time", timeformat: "%m/%d" },
+        legend: { position: "nw", noColumns: 2, margin: 10}
     });
     function update() {
-	console.log("update chart at: ", Date());
-	var data = getData()
-	plotStrawberry.setData(data[0]);
-	plotStrawberry.draw();
-	plotCarrot.setData(data[1]);
-	plotCarrot.draw();
-	setTimeout(update, updateInterval);
+        console.log("update chart at: ", Date());
+        var data = getData()
+        plotStrawberry.setData(data[0]);
+        plotStrawberry.draw();
+        setTimeout(update, updateInterval);
     }
     update();
 });
